05893a82b95d12b5b5b1ea9afc62ac64966622fc
wre0rl <wre0rl@gmail.com>
2023-05-17 20:09:09 +0700

    Add resize logic and change the code structure

--- index.js
+++ index.js
@@ -1,49 +1,75 @@
-const ROW = 64;
-const COLUMN = 64;
+const sketchContainer  = document.querySelector('.sketch-container');
 
-const sketchContainer  = document.querySelector('.sketch-container ');
+let grids ;
 
-for (let i = 1; i <= COLUMN; i++) {
-  const column = document.createElement('div');
-  column.setAttribute('class', 'column');
-  column.setAttribute('id', 'column-' + i);
-  sketchContainer.appendChild(column);
-  
-  for (let i = 0; i < ROW; i++) {
-    const div = document.createElement('div');
-    div.setAttribute('class', 'grid');
-    div.setAttribute('id', 'grid-' + i);
-    column.appendChild(div);
+function drawGrids(row, col) {
+  for (let i = 1; i <= col; i++) {
+    const column = document.createElement('div');
+    column.setAttribute('class', 'column');
+    column.setAttribute('id', 'column-' + i);
+    sketchContainer.appendChild(column);
+    
+    for (let i = 0; i < row; i++) {
+      const div = document.createElement('div');
+      div.setAttribute('class', 'grid');
+      div.setAttribute('id', 'grid-' + i);
+      column.appendChild(div);
+    }
   }
 }
 
 // Change the grid color by clicking and hovering the mouse
-const grid = document.querySelectorAll('.grid');
+function getGrids() {
+  grids = document.querySelectorAll('.grid');
 
-let isMouseDown = false;
+  let isMouseDown = false;
 
-grid.forEach(el => {
-  el.addEventListener('mousedown', () => {
-    isMouseDown = true;
-    el.classList.add('trail');
+  grids.forEach(grid => {
+    grid.addEventListener('mousedown', () => {
+      isMouseDown = true;
+      grid.classList.add('trail');
+    });
+
+    grid.addEventListener('mousemove', () => {
+      if (isMouseDown) {
+        grid.classList.add('trail');
+      }
+    });
+
+    grid.addEventListener('mouseup', () => {
+      isMouseDown = false;
+    });
   });
+}
 
-  el.addEventListener('mousemove', () => {
-    if (isMouseDown) {
-      el.classList.add('trail');
-    }
-  });
+function loadGrids(row, col) {
+  drawGrids(row, col);
+  getGrids();
+}
 
-  el.addEventListener('mouseup', () => {
-    isMouseDown = false;
-  });
+loadGrids(64, 64); // First load
+
+
+// Resize the grids
+const resize = document.querySelector('#resize');
+
+resize.addEventListener('click', () => {
+  let size = +(prompt('Type the grid sizes.'));
+  //drawGrids(size, size);
+  if (size) { // Check if it's a number
+    sketchContainer.textContent = ''; // Remove the sketch child element DOM
+    loadGrids(size, size);
+  } else {
+    console.log('NaN');
+  }
 });
 
-// Clear the grid
+// Clear the grids
 const clear = document.querySelector('#clear');
 
 clear.addEventListener('click', () => {
-  grid.forEach(el => {
-    el.classList.remove('trail');
+  grids = document.querySelectorAll('.grid');
+  grids.forEach(grid => {
+    grid.classList.remove('trail');
   });
 });
