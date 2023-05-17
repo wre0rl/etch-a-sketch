72393aedcd7c7efc18e0d80f10040454a82b2894
wre0rl <wre0rl@gmail.com>
2023-05-17 17:03:51 +0700

    Added container in html, container and grid styling, and javascript logic for dynamically creating column and row

---
+++ index.js
@@ -0,0 +1,19 @@
+const ROW = 16;
+const COLUMN = 16;
+
+const container = document.querySelector('.container');
+
+
+for (let i = 1; i <= COLUMN; i++) {
+  const column = document.createElement('div');
+  column.setAttribute('class', 'column');
+  column.setAttribute('id', 'column-' + i);
+  container.appendChild(column);
+  
+  for (let i = 0; i < ROW; i++) {
+    const div = document.createElement('div');
+    div.setAttribute('class', 'grid');
+    div.setAttribute('id', 'grid-' + i);
+    column.appendChild(div);
+  }
+}
