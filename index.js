99711bb878557fded458cbe4f70303e139b6713d
wre0rl <wre0rl@gmail.com>
2023-05-17 18:25:26 +0700

    Changed container into sketch container and add new setting container

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const ROW = 16;
 const COLUMN = 16;
 
-const container = document.querySelector('.container');
+const sketchContainer  = document.querySelector('.sketch-container ');
 
 for (let i = 1; i <= COLUMN; i++) {
   const column = document.createElement('div');
   column.setAttribute('class', 'column');
   column.setAttribute('id', 'column-' + i);
-  container.appendChild(column);
+  sketchContainer.appendChild(column);
   
   for (let i = 0; i < ROW; i++) {
     const div = document.createElement('div');
